8de5263ce7b1f228077d9a58af0521758bf847f5
h1zardian <amanb.singh21@gmail.com>
2026-08-01 23:44:28 +0530

    chore(infra): add official GitHub OIDC thumbprints to IAM OIDC provider

--- infra/terraform/modules/oidc/main.tf
+++ infra/terraform/modules/oidc/main.tf
@@ -3,9 +3,12 @@ data "tls_certificate" "github" {
 }
 
 resource "aws_iam_openid_connect_provider" "github" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
+  url            = "https://token.actions.githubusercontent.com"
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a21860c07ebb1496735510619a97eb943615"
+  ]
 }
 
 data "aws_iam_policy_document" "github_assume_role" {
