73bb59c5ff31f0e5ab2c31783f24d2fe4cffb1d3
h1zardian <amanb.singh21@gmail.com>
2026-08-02 11:03:17 +0530

    fix(iam): add s3:DeleteObject for Terraform state lock release

--- infra/terraform/modules/oidc/main.tf
+++ infra/terraform/modules/oidc/main.tf
@@ -90,7 +90,7 @@ data "aws_iam_policy_document" "terraform_provisioner" {
     sid    = "S3AndDynamoDBBackendPermissions"
     effect = "Allow"
     actions = [
-      "s3:GetObject", "s3:PutObject", "s3:ListBucket",
+      "s3:GetObject", "s3:PutObject", "s3:DeleteObject", "s3:ListBucket",
       "dynamodb:GetItem", "dynamodb:PutItem", "dynamodb:DeleteItem"
     ]
     resources = ["*"]
